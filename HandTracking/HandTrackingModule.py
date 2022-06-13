e760e33c7ba7e7f8654df057f9ca6c3b1849e798
Kyle Sargeant <Sargeakb9348@uwec.edu>
2022-06-13 17:04:42 -0500

    transfer from pycharm

---
+++ HandTracking/HandTrackingModule.py
@@ -0,0 +1,65 @@
+import cv2
+import mediapipe as mp
+import time
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=6, modelComplexity=1, minDetectionConfidence=0.75, minTrackingConfidence=0.75):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComplexity = modelComplexity
+        self.minDetectionConfidence = minDetectionConfidence
+        self.minTrackingConfidence=minTrackingConfidence
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.minDetectionConfidence, self.minTrackingConfidence)  # default parameters are fine
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self,img,draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self,img,handNum=0, draw=True):
+
+        lmList=[]
+        if self.results.multi_hand_landmarks:
+
+            myHand=self.results.multi_hand_landmarks[handNum]
+
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)  # center location
+                #print(id, cx, cy)
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+
+        return lmList
+def main():
+    previousTime = 0
+    currentTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+
+    while True:
+        success, img = cap.read()
+        img=detector.findHands(img)
+        lmList=detector.findPosition(img)
+        if len(lmList) !=0:
+            print(lmList[0])#print location of lmList["landmark"] 0 = wrist, 4 = thumb, 8 = index, 20 = pinky
+        currentTime = time.time()
+        fps = 1 / (currentTime - previousTime)
+        previousTime = currentTime
+
+        cv2.putText(img, str(int(fps)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
---
+++ HandTracking/MyNewGameHandTracking.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+
+previousTime = 0
+currentTime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+
+while True:
+    success, img = cap.read()
+    img=detector.findHands(img)
+    lmList=detector.findPosition(img)
+    if len(lmList) !=0:
+        print(lmList[0])#print location of lmList["landmark"] 0 = wrist, 4 = thumb, 8 = index, 20 = pinky
+    currentTime = time.time()
+    fps = 1 / (currentTime - previousTime)
+    previousTime = currentTime
+
+    cv2.putText(img, str(int(fps)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)

8e8f9e50a38ac31dc7b8961904648a61c6826397
Kyle Sargeant <Sargeakb9348@uwec.edu>
2022-08-10 18:51:42 -0500

    Implemented simple finger track and click

--- HandTracking/HandTrackingModule.py
+++ HandTracking/HandTrackingModule.py
@@ -4,18 +4,21 @@ import cv2
 import mediapipe as mp
 import time
 
+
 class handDetector():
-    def __init__(self, mode=False, maxHands=6, modelComplexity=1, minDetectionConfidence=0.75, minTrackingConfidence=0.75):
+    def __init__(self, mode=False, maxHands=6, modelComplexity=1, minDetectionConfidence=0.75,
+                 minTrackingConfidence=0.75):
         self.mode = mode
         self.maxHands = maxHands
         self.modelComplexity = modelComplexity
         self.minDetectionConfidence = minDetectionConfidence
-        self.minTrackingConfidence=minTrackingConfidence
+        self.minTrackingConfidence = minTrackingConfidence
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.minDetectionConfidence, self.minTrackingConfidence)  # default parameters are fine
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.minDetectionConfidence,
+                                        self.minTrackingConfidence)  # default parameters are fine
         self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self,img,draw = True):
+    def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
         # print(results.multi_hand_landmarks)
@@ -26,47 +29,53 @@ class handDetector():
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
 
-    def findPosition(self,img,handNum=0, draw=True):
+    def findPosition(self, img, handNum=0, draw=True):
         xList = []
-        yList= []
-        self.lmList=[]
+        yList = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
 
-            myHand=self.results.multi_hand_landmarks[handNum]
+            myHand = self.results.multi_hand_landmarks[handNum]
 
             for id, lm in enumerate(myHand.landmark):
                 # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)  # center location
-                #print(id, cx, cy)
+                # print(id, cx, cy)
                 xList.append(cx)
                 yList.append(cy)
-                self.lmList.append([id,cx,cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
 
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
-            bbox = xmin, ymin, xmax, ymax
 
-            if draw:
-                cv2.rectangle(img, (xmin-20, ymin-20), (xmax+20, ymax+20), (0,255,0),2)
-        return self.lmList, bbox
+        return (self.lmList)
 
-def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
-    x1, y1 = self.lmList[p1][1:0]
-    x2, y2 = self.lmList[p2][1:0]
-    cx, cy = (x1+x2)//2, (y1+y2)//2
+    # def findDistance(self, p1, p2, img, draw=False, r=15, t=3):
+    #     x1, y1 = self.lmList[p1][1:0]
+    #
+    #     x2, y2 = self.lmList[p2][1:0]
+    #
+    #     cx, cy = (x1 + x2) // 2, (y1 + y2) // 2 #midpoint
+    #
+    #     if draw:
+    #         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), t)
+    #         cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+    #         cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+    #         cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+    #
+    #
+    #     length = math.hypot(x2 - x1, y2 - y1)
+    #
+    #     return length, img, [x1, y1, x2, y2, cx, cy]
 
-    if draw:
-        cv2.line(img, (x1,y1),(x2,y2),(0,255,0),t)
-        cv2.circle(img,(x1,y1), r, (255,0,255),cv2.FILLED)
-        cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+    def findDistance(self,p1,p2):
+        x1, y1 = self.lmList[p1][1:0]
 
-    length = math.hypot(x2-x1, y2-y1)
+        x2, y2 = self.lmList[p2][1:0]
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length
 
-    return length,img,[x1,y1,x2,y2,cx,cy]
 
 def main():
     previousTime = 0
@@ -76,18 +85,19 @@ def main():
 
     while True:
         success, img = cap.read()
-        img=detector.findHands(img)
-        lmList=detector.findPosition(img)
-        if len(lmList) !=0:
-            print(lmList[0])#print location of lmList["landmark"] 0 = wrist, 4 = thumb, 8 = index, 20 = pinky
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[0])  # print location of lmList["landmark"] 0 = wrist, 4 = thumb, 8 = index, 20 = pinky
 
         currentTime = time.time()
         fps = 1 / (currentTime - previousTime)
         previousTime = currentTime
-
         cv2.putText(img, str(int(fps)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+
         cv2.imshow("Image", img)
         cv2.waitKey(1)
+
 
 if __name__ == "__main__":
     main()

44e59ec9ed81829c167a4a9b1a9ae57a33a81270
Kyle Sargeant <Sargeakb9348@uwec.edu>
2022-08-10 16:56:08 -0500

    added functions to HandTrackingModule.py

--- HandTracking/HandTrackingModule.py
+++ HandTracking/HandTrackingModule.py
@@ -1,3 +1,5 @@
+import math
+
 import cv2
 import mediapipe as mp
 import time
@@ -25,8 +27,9 @@ class handDetector():
         return img
 
     def findPosition(self,img,handNum=0, draw=True):
-
-        lmList=[]
+        xList = []
+        yList= []
+        self.lmList=[]
         if self.results.multi_hand_landmarks:
 
             myHand=self.results.multi_hand_landmarks[handNum]
@@ -36,11 +39,35 @@ class handDetector():
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)  # center location
                 #print(id, cx, cy)
-                lmList.append([id,cx,cy])
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id,cx,cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
 
-        return lmList
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (xmin-20, ymin-20), (xmax+20, ymax+20), (0,255,0),2)
+        return self.lmList, bbox
+
+def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+    x1, y1 = self.lmList[p1][1:0]
+    x2, y2 = self.lmList[p2][1:0]
+    cx, cy = (x1+x2)//2, (y1+y2)//2
+
+    if draw:
+        cv2.line(img, (x1,y1),(x2,y2),(0,255,0),t)
+        cv2.circle(img,(x1,y1), r, (255,0,255),cv2.FILLED)
+        cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+
+    length = math.hypot(x2-x1, y2-y1)
+
+    return length,img,[x1,y1,x2,y2,cx,cy]
+
 def main():
     previousTime = 0
     currentTime = 0
@@ -53,6 +80,7 @@ def main():
         lmList=detector.findPosition(img)
         if len(lmList) !=0:
             print(lmList[0])#print location of lmList["landmark"] 0 = wrist, 4 = thumb, 8 = index, 20 = pinky
+
         currentTime = time.time()
         fps = 1 / (currentTime - previousTime)
         previousTime = currentTime
